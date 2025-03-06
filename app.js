cd0485f031cf40f5615dd5b01eff7525f0d10d01
geekprathamesh <prathameshteli727@gmail.com>
2025-03-06 20:42:39 +0530

    added code

--- app.js
+++ app.js
@@ -1,41 +1,40 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelectorAll("#reset-btn");
 
-let turn0=true;   //player x gives o
+let turnx = true; //player x and player y
 
 // let arr=["banana","apple","chiku"]; //1d array
 
 // let arr2=[["banana","mango"],["chiku","potato"]]  //2d array
 
+const winPatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
-const winPatterns=[
-    [0,1,2],
-    [0,3,6],
-    [3,4,5],
-    [6,7,8],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-]
+// 2d arrays
 
-boxes.forEach((box) =>{
-    box.addEventListener("click",()=>{
-        console.log("box was clicked");
-        if(turn0){
-            box.innerText="O";
-            turn0=false;
-        }
-        else{
-            box.innerText="X";
-            turn0=true;
-        }
-        box.disabled=true;
-    })
+for (let key of boxes) {
+  key.addEventListener("click", () => {
+    console.log("box was clicked");
+    if (turnx) {
+      key.innerText = "x";
+      turnx = false;
+    } else {
+      key.innerText = "o";
+      turnx = true;
+    }
+  });
 }
-checkwinner();
-)
 
-const checkwinner =()=>{
-    
-}
+// boxes.forEach((box)=>{
+//     box.addEventListener("click",()=>{
+//         console.log("clicked")
+//     })
+// })
